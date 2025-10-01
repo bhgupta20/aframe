4bf33def8650580024a55b4eb3f769d100b33557
bhavyagupta <bhavyagupta20112001@gmail.com>
2025-10-01 13:38:21 -0400

    add decimator option to the datamodule

--- projects/train/train/data/base.py
+++ projects/train/train/data/base.py
@@ -163,6 +163,7 @@ class BaseAframeDataset(pl.LightningDataModule):
         waveform_prob: float = 1,
         left_pad: float = 0,
         right_pad: float = 0,
+        schedule: Optional[List[List[int]]] = None,
         fftlength: Optional[float] = None,
         highpass: Optional[float] = None,
         lowpass: Optional[float] = None,
